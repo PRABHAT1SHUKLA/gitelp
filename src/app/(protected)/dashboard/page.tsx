7a9c3d30707a6a18a0dcc74089de3a436b9532ae
prabhat <prabhatshukla0849@gmail.com>
2025-02-11 16:23:50 +0530

    feat: dashboard page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,13 +1,41 @@
 "use client"
 
+import useProject from "@/hooks/use-project"
 import { useUser } from "@clerk/nextjs"
+import { ExternalLink, Github } from "lucide-react"
+import Link from "next/link"
 
 
-const DashboardPage = () =>{
-  const {user } = useUser()
+const DashboardPage = () => {
 
-  return(
-    <div> {user?.firstName}</div>
+  const { project } = useProject()
+
+  return (
+    <div>
+      <div className="flex items-center justify-between flex-wrap gap-y-4">
+        <div className="w-fit rounded-md bg-gray-950 px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white " />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+
+                This project is linked to {' '}
+                <Link href={project?.githubUrl ?? ""} className="inline-flex items-center text-white/80 hover:underline">
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+
+              </p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
