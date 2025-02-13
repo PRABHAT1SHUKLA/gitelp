e834042fbdcd2ce23f2efc96b7cb26af199b0a9c
prabhat <prabhatshukla0849@gmail.com>
2025-02-13 16:55:05 +0530

    function to generate for all files recursively implemented

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,5 +1,6 @@
 import {GithubRepoLoader} from "@langchain/community/document_loaders/web/github"
 import { Document } from "@langchain/core/documents"
+import { generateEmbedding, summariseCode } from "./gemini"
 
 export const loadGithubRepo = async(githubUrl:string,githubToken?: string ) => {
   const loader = new GithubRepoLoader(githubUrl, {
@@ -22,6 +23,21 @@ export const indexGithubRepo = async (projectId: string , githubUrl : string , g
     githubToken
   )
 
-  
+
 
 } 
+
+
+const generateEmbeddings = async(docs : Document[]) =>{
+      return await Promise.all(docs.map(async doc => {
+        const summary = await summariseCode(doc)
+        const embedding = await generateEmbedding(summary)
+
+        return {
+          summary,
+          embedding,
+          sourceCode : JSON.parse(JSON.stringify(doc.pageContent)),
+          filename : doc.metadata.source
+        }
+      }))
+}
