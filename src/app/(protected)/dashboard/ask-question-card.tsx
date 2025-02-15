78037f09608529620810118a2dc34503ed2ad1ed
prabhat <prabhatshukla0849@gmail.com>
2025-02-15 18:15:29 +0530

    react-md-editor for dialog box beautification

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import MDEditor from '@uiw/react-md-editor'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Textarea } from '@/components/ui/textarea'
@@ -20,13 +21,16 @@ const AskQuestionCArd = () => {
 
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setAnswer('')
+    setFilesReferences([])
     e.preventDefault()
     if(!project?.id) return
     setLoading(true)
-    setOpen(true)
+ 
 
 
     const {output , filesReferences} = await askQuestion(question, project.id)
+    setOpen(true)
     setFilesReferences(filesReferences)
     
     for await (const delta of readStreamableValue(output) ){
@@ -43,18 +47,18 @@ const AskQuestionCArd = () => {
       <Dialog open={open} onOpenChange={setOpen}
       >
 
-        <DialogContent>
+        <DialogContent className='sm:max-w-[80vw]'>
           <DialogHeader>
             <DialogTitle>
               <Image src='/logo.jpg' alt="gitelp" width={40} height={40} />
             </DialogTitle>
           </DialogHeader>
 
-          {answer}
-          <h1> File References</h1>
-          {filesReferences.map(file => {
-            return <span>{file.fileName}</span>
-          })}
+        <MDEditor.Markdown source={answer} className='max-w-[70vw] !h-full max-h-[40vh] overflow-scroll' />
+
+         <Button type='button' onClick={()=>{setOpen(false)}}>
+            Close
+         </Button>
 
         </DialogContent>
 
@@ -69,7 +73,7 @@ const AskQuestionCArd = () => {
 
             <div className="h-4"></div>
 
-            <Button type='submit'>
+            <Button type='submit' disabled={loading}>
               Ask Gitelp
             </Button>
 
