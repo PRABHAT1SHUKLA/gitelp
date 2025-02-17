06cb42cf26c7f63e223033d98b9087a12283adf0
prabhat <prabhatshukla0849@gmail.com>
2025-02-17 06:38:06 +0530

    feat : added firebase config function to be called on ui to track progress of upload related task

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,36 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFile(file: File, setProgress?: (progress: number) => void) {
+
+  return new Promise((resolve, reject) => {
+    try {
+      const storageRef = ref(storage, file.name)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on('state_changed', snapshot => {
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+        if (setProgress) setProgress(progress)
+
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('upload is paused'); break;
+          case 'running':
+            console.log('upload is running'); break;
+        }
+      }, error => {
+        reject(error)
+      }, () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadUrl => {
+          resolve(downloadUrl)
+        })
+      })
+    } catch (error) {
+      console.error(error)
+      reject(error)
+    }
+  })
+
+}
