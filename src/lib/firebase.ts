99e298b9dcac84785e12c3c5bffa08b02520dc9f
prabhat <prabhatshukla0849@gmail.com>
2025-02-17 06:19:11 +0530

    feat: added firebase sdk and initialized it for gitelp

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDY_6Q6HvMPMCtxW3JlnN3e-C2W6C6rXsw",
+  authDomain: "gitelp-1024e.firebaseapp.com",
+  projectId: "gitelp-1024e",
+  storageBucket: "gitelp-1024e.firebasestorage.app",
+  messagingSenderId: "233137051276",
+  appId: "1:233137051276:web:1acff8e878c089d00a0da9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
