8c70f225131dfa8da0d8e0121507de365caa8666
prabhat <prabhatshukla0849@gmail.com>
2025-02-21 14:35:22 +0530

    user allowed for creating project if only sufficient credits are available

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import useRefetch from '@/hooks/use-refetch'
 import { api } from '@/trpc/react'
+import { Info } from 'lucide-react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -14,29 +15,45 @@ type FormInput = {
   githubToken?: string
 }
 
+
 const CreatePage = () => {
 
   const { register, handleSubmit, reset } = useForm<FormInput>()
 
   const createProject = api.project.createProject.useMutation()
 
+  const checkCredits = api.project.checkCredits.useMutation()
+
   const refetch = useRefetch()
 
   async function onSubmit(data: FormInput) {
-    try {
-      await createProject.mutateAsync({
-        githubUrl: data.repoUrl,
-        name: data.projectName,
-        githubToken: data.githubToken,
-      });
 
-      toast.success("Project created Successfully");
-      await refetch();
-      reset();
-    } catch (error) {
-      toast.error("Error creating project");
+    if (!!checkCredits.data) {
+      try {
+        await createProject.mutateAsync({
+          githubUrl: data.repoUrl,
+          name: data.projectName,
+          githubToken: data.githubToken,
+        });
+
+        toast.success("Project created Successfully");
+        await refetch();
+        reset();
+      } catch (error) {
+        toast.error("Error creating project");
+      }
+    } else {
+      checkCredits.mutate({
+        githubUrl: data.repoUrl,
+        githubToken: data.githubToken
+      })
     }
-  }
+}
+
+const hasEnoughCredits = checkCredits?.data?.userCredits ? checkCredits.data.fileCount <= checkCredits.data.fileCount : true
+
+
+
   return (
     <div className='flex items-center gap-12 h-full justify-center'>
       <img src='/git.svg' className='h-56 w-auto' />
@@ -75,14 +92,29 @@ const CreatePage = () => {
             <Input
               {...register('githubToken')}
               placeholder="Github Token (optional)"
-          
+
             />
 
+            {
+              !!checkCredits.data && (
+                <>
+                  <div className="mt-4 bg-orange-50 px-4 py-2 rounded-md border border-orange-200 text-orange-700">
+                    <div className="flex items-center gap-2">
+                      <Info className='size-4' />
+                      <p className='text-sm'>You will be charged <strong>{checkCredits.data?.fileCount}</strong> credits for this repository.</p>
+                    </div>
+                    <p className='text-sm text-blue-800 ml-6'>You have <strong>{checkCredits.data?.userCredits} </strong>credits remaining.</p>
+                  </div>
+                </>
+              )
+            }
+
             <div className='h-4'></div>
-           
-           <Button type="submit" disabled={createProject.isPending}>
-             Create Project
-           </Button>
+
+            <Button type="submit" disabled={createProject.isPending || checkCredits.isPending || !hasEnoughCredits}>
+              {!!checkCredits.data ? 'Create Project ' : 'Check Credits'}
+              Create Project
+            </Button>
 
           </form>
         </div>
